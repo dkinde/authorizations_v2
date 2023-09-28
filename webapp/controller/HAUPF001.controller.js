ef5d97ee80793e55b03eff03e1ed1cbac5db2b0c
dkinde <dkindelancastro@gmail.com>
2023-09-28 18:25:40 +0200

    filterbar - personalfkt und funktionwert

--- webapp/controller/HAUPF001.controller.js
+++ webapp/controller/HAUPF001.controller.js
@@ -34,10 +34,197 @@ sap.ui.define([
             this.mGroupFunctions = {};
             sap.ui.getCore().getConfiguration().setLanguage("de");
 
+            this.aValue = [];
+            this._oPage = this.byId("dynamicPage1");
+
+            var that = this,
+                iSkip = 0;
+
+            function getData() {
+                $.ajax({
+                    url: that.getOwnerComponent().getModel().sServiceUrl + "/HAUPF001" + "?$top=500" + "&$skip=" + iSkip,
+                    method: "GET",
+                    success: function (data) {
+                        if (data && data.value) {
+                            that.aValue = that.aValue.concat(data.value.map(function (item) {
+                                return item;
+                            }));
+                        }
+                        if (data.value.length === 500) {
+                            iSkip += 500;
+                            getData();
+                        } else {
+                            var aDistinctItems = that.aValue.reduce(function (aUnique, oItem) {
+                                if (!aUnique.some(function (obj) { return obj.personalnummer === oItem.personalnummer; })) {
+                                    aUnique.push(oItem);
+                                }
+                                return aUnique;
+                            }, []);
+
+                            var aDistinctItems1 = that.aValue.reduce(function (aUnique, oItem) {
+                                if (!aUnique.some(function (obj) { return obj.funktion === oItem.funktion; })) {
+                                    aUnique.push(oItem);
+                                }
+                                return aUnique;
+                            }, []);
+
+                            var oDistinctModel = new sap.ui.model.json.JSONModel({
+                                distinctItems: aDistinctItems
+                            });
+                            var oDistinctModel1 = new sap.ui.model.json.JSONModel({
+                                distinctItems1: aDistinctItems1
+                            });
+
+                            that.getView().byId("multiPersonal").setModel(oDistinctModel);
+                            that.getView().byId("multiFunktion").setModel(oDistinctModel1);
+
+
+                            that._oPage.setBusy(false);
+                            console.log("that.aValue:" + that.aValue.length);
+                            console.log("aDistinctItems:" + aDistinctItems.length);
+                            console.log("aDistinctItems1:" + aDistinctItems1.length);
+
+                            return;
+                        }
+                    }.bind(this),
+                    error: function (errorEntit1) {
+                        console.log("Fehler bei der Abfrage von Entität 1:", errorEntit1);
+                    }
+                });
+            }
+            getData();
+
+            /* this.oSmartVariantManagement = this.getView().byId("svm"); */
+            this.oFilterBar = this.getView().byId("filterbar");
+            this.oExpandedLabel = this.getView().byId("expandedLabel");
+            this.oSnappedLabel = this.getView().byId("snappedLabel");
+            this.oTable = this.getView().byId("table1");
+            this.applyData = this.applyData.bind(this);
+            this.fetchData = this.fetchData.bind(this);
+            this.getFiltersWithValues = this.getFiltersWithValues.bind(this);
+
+            this.oFilterBar.registerFetchData(this.fetchData);
+            this.oFilterBar.registerApplyData(this.applyData);
+            this.oFilterBar.registerGetFiltersWithValues(this.getFiltersWithValues);
+
+            /* var oPersInfo = new sap.ui.comp.smartvariants.PersonalizableInfo({
+                type: "filterBar",
+                keyName: "persistencyKey",
+                dataSource: "",
+                control: this.oFilterBar
+            });
+            this.oSmartVariantManagement.addPersonalizableControl(oPersInfo);
+            this.oSmartVariantManagement.initialise(function () { }, this.oFilterBar); */
+
         },
         onNavButtonPressed: function () {
             var oRouter = UIComponent.getRouterFor(this);
             oRouter.navTo("RouteFunktion");
+        },
+        fetchData: function () {
+            var aData = this.oFilterBar.getAllFilterItems().reduce(function (aResult, oFilterItem) {
+                aResult.push({
+                    groupName: oFilterItem.getGroupName(),
+                    fieldName: oFilterItem.getName(),
+                    fieldData: oFilterItem.getControl().getSelectedKeys()
+                });
+
+                return aResult;
+            }, []);
+
+            return aData;
+        },
+        applyData: function (aData) {
+            aData.forEach(function (oDataObject) {
+                var oControl = this.oFilterBar.determineControlByName(oDataObject.fieldName, oDataObject.groupName);
+                oControl.setSelectedKeys(oDataObject.fieldData);
+            }, this);
+        },
+        getFiltersWithValues: function () {
+            var aFiltersWithValue = this.oFilterBar.getFilterGroupItems().reduce(function (aResult, oFilterGroupItem) {
+                var oControl = oFilterGroupItem.getControl();
+
+                if (oControl && oControl.getSelectedKeys && oControl.getSelectedKeys().length > 0) {
+                    aResult.push(oFilterGroupItem);
+                }
+
+                return aResult;
+            }, []);
+
+            return aFiltersWithValue;
+        },
+        onSelectionChange: function (oEvent) {
+            //this.oSmartVariantManagement.currentVariantSetModified(true);
+            this.oFilterBar.fireFilterChange(oEvent);
+        },
+        onFilterChange: function () {
+            this._updateLabelsAndTable();
+        },
+        onAfterVariantLoad: function () {
+            this._updateLabelsAndTable();
+        },
+        _updateLabelsAndTable: function () {
+            this.oExpandedLabel.setText(this.getFormattedSummaryTextExpanded());
+            this.oSnappedLabel.setText(this.getFormattedSummaryText());
+            this.oTable.setShowOverlay(true);
+        },
+        getFormattedSummaryText: function () {
+            var aFiltersWithValues = this.oFilterBar.retrieveFiltersWithValues();
+
+            if (aFiltersWithValues.length === 0) {
+                return "No filters active";
+            }
+
+            if (aFiltersWithValues.length === 1) {
+                return aFiltersWithValues.length + " filter active: " + aFiltersWithValues.join(", ");
+            }
+
+            return aFiltersWithValues.length + " filters active: " + aFiltersWithValues.join(", ");
+        },
+        getFormattedSummaryTextExpanded: function () {
+            var aFiltersWithValues = this.oFilterBar.retrieveFiltersWithValues();
+
+            if (aFiltersWithValues.length === 0) {
+                return "No filters active";
+            }
+
+            var sText = aFiltersWithValues.length + " filters active",
+                aNonVisibleFiltersWithValues = this.oFilterBar.retrieveNonVisibleFiltersWithValues();
+
+            if (aFiltersWithValues.length === 1) {
+                sText = aFiltersWithValues.length + " filter active";
+            }
+
+            if (aNonVisibleFiltersWithValues && aNonVisibleFiltersWithValues.length > 0) {
+                sText += " (" + aNonVisibleFiltersWithValues.length + " hidden)";
+            }
+
+            return sText;
+        },
+        onSearch: function () {
+            var aTableFilters = this.oFilterBar.getFilterGroupItems().reduce(function (aResult, oFilterGroupItem) {
+                var oControl = oFilterGroupItem.getControl(),
+                    aSelectedKeys = oControl.getSelectedKeys(),
+                    aFilters = aSelectedKeys.map(function (sSelectedKey) {
+                        return new sap.ui.model.Filter({
+                            path: oFilterGroupItem.getName(),
+                            operator: sap.ui.model.FilterOperator.Contains,
+                            value1: sSelectedKey
+                        });
+                    });
+
+                if (aSelectedKeys.length > 0) {
+                    aResult.push(new sap.ui.model.Filter({
+                        filters: aFilters,
+                        and: false
+                    }));
+                }
+
+                return aResult;
+            }, []);
+
+            this.oTable.getBinding("items").filter(aTableFilters, sap.ui.model.FilterType.Application);
+            this.oTable.setShowOverlay(false);
         },
         onCloseViewDialog: function () {
             this._oModel.resetChanges();
@@ -284,7 +471,7 @@ sap.ui.define([
                 sap.m.MessageBox.warning("kein Element zum Löschen ausgewählt");
             }
         },
-        onSearch: function (oEvent) {
+        onSearch1: function (oEvent) {
             var aFilters = [],
                 sQuery = oEvent.getSource().getValue();
 
